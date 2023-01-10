d8614a81fa66f12420eab3123e490606a4996b93
Michael Kreil <github@michael-kreil.de>
2023-01-10 17:38:09 +0100

    8 cores are slow, but efficient

--- bin/run_google_compute/2_convert_tiles.sh
+++ bin/run_google_compute/2_convert_tiles.sh
@@ -3,14 +3,14 @@ cd "$(dirname "$0")"
 
 set -ex
 
-name="2023-01-eu-de"
+name="2023-01-eu"
 tile_src="gs://opencloudtiles/mbtiles/$name.mbtiles"
 tile_dst="gs://opencloudtiles/cloudtiles/$name.cloudtiles"
 
 #machine_type="n2d-highcpu-2"     # 2 cores
 #machine_type="n2d-highcpu-4"     # 4 cores
-#machine_type="n2d-highcpu-8"     # 8 cores
-machine_type="n2d-highcpu-16"    # 16 cores
+machine_type="n2d-highcpu-8"     # 8 cores
+#machine_type="n2d-highcpu-16"    # 16 cores
 #machine_type="n2d-highcpu-32"    # 32 cores
 #machine_type="n2d-highcpu-48"    # 48 cores
 #machine_type="n2d-highcpu-80"    # 80 cores
@@ -38,7 +38,7 @@ command="$command; gsutil cp $tile_src ."
 command="$command; opencloudtiles convert --precompress brotli $(basename $tile_src) $(basename $tile_dst)"
 command="$command; gsutil cp $(basename $tile_dst) $tile_dst"
 
-gcloud compute ssh opencloudtiles-converter --command="$command"
+gcloud compute ssh opencloudtiles-converter --command="$command" -- -t
 
 gcloud compute instances stop opencloudtiles-converter --quiet
 
